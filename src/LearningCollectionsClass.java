17b1bf38c105c4020252216bd0674617a00be8fa
aiqqia <aiqqia.ag@gmail.com>
2021-06-24 14:48:32 +0530

    Added Classes

---
+++ src/LearningCollectionsClass.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LearningCollectionsClass {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(34);
+        list.add(12);
+        list.add(9);
+        list.add(9);
+        list.add(9);
+        list.add(76);
+        list.add(29);
+        list.add(75);
+
+        System.out.println("min element " + Collections.min(list));
+        System.out.println("max element " + Collections.max(list));
+        System.out.println(Collections.frequency(list, 9));
+
+        Collections.sort(list);
+        //Collections.sort(list, Comparator.reverseOrder()); //descending
+
+        System.out.println(list);
+
+    }
+
+}
