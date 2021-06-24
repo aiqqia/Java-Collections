db4aab503b1199b282b9f4efd10f0068008e9a8b
aiqqia <aiqqia.ag@gmail.com>
2021-06-24 14:50:33 +0530

    Added Student Class

--- src/LearningCollectionsClass.java
+++ src/LearningCollectionsClass.java
@@ -26,6 +26,24 @@ public class LearningCollectionsClass {
 
         System.out.println(list);
 
+        /*List<Student> list = new ArrayList<>();
+
+        list.add(new Student("Anuj", 2));
+        list.add(new Student("Ramesh", 4));
+        list.add(new Student("Shivam", 3));
+        list.add(new Student("Rohit", 1));
+
+        Student s1 = new Student("Anuj", 2);
+        Student s2 = new Student("Rohit", 3);
+
+//        System.out.println((s1.compareTo(s2)));
+
+        System.out.println(list);
+
+        Collections.sort(list, (o1, o2) -> o1.name.compareTo(o2.name));
+
+        System.out.println(list);*/
+
     }
 
 }
